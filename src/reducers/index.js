9515056862b0eb8c4299e23b5d57599653fd67e6
stofstik <verstegen.daan@gmail.com>
2016-11-04 12:02:18 +0100

    fixed merge mistakes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,6 @@ import loading from './loading'
 import player from './player'
 import players from './players'
 import playerProfileId from './player-profile-id'
-import errors from './errors'
-import loading from './loading'
-import player from './player'
 
 module.exports = {
   errors,
